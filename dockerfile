9e3ad279b05d318e5f09fb437898afd27a17fe58
Hanjun Kwon <kwon96718@gmail.com>
2022-05-24 16:33:25 +0900

    fix: error fix

--- dockerfile
+++ dockerfile
@@ -17,8 +17,8 @@ RUN rm -rf /etc/localtime; \
   ln -s /usr/share/zoneinfo/Asia/Seoul /etc/localtime;
 
 ENV NODE_ENV production
-ENV PORT 3000
+ENV PORT 4000
 
-EXPOSE 3000
+EXPOSE 4000
 
 CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { toDB, register } = require('./libs/toDB');
 const cors = require('cors');
 const routerMonitor = require('./routes/monitoring');
 const routerLogin = require('./routes/login');
+const path = require('path');
 const app = express();
 
 require('dotenv').config();
@@ -14,10 +15,19 @@ const PASSWORD = process.env.PASSWORD;
 register(USERNAME, PASSWORD);
 setInterval(() => toDB(), 10000);
 
-app.listen(3000);
-app.use(morgan('dev'));
-app.use(cors());
+app.listen(4000);
+app.use(morgan('common'));
+app.use(cors({origin:"http://localhost:4000",credentials:true}));
 app.use(express.json());
 
 app.use('/monitor', routerMonitor);
 app.use('/login', routerLogin);
+
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/build/index.html'));
+});
+app.get('*', function (req,res) {
+  res.sendFile(path.join(__dirname, '/build/index.html'));
+});
